e2d652b0c4e4e086c22a94118557848f94aa6784
Marat Isaw <marat.isaw23@gmail.com>
2022-12-03 22:54:29 +0100

    different colors

--- public/client.js
+++ public/client.js
@@ -1,41 +1,53 @@
+let ctx;
+let socket;
+let color;
+
 $(() => {
+  setup();
+});
+
+function setup() {
   // setup
-  console.log("Loading canvas...");
+  console.log("Loading canvas and context...");
   const canvas = document.querySelector("#canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.addEventListener("click", click, false);
 
   ctx = canvas.getContext("2d");
 
-  ctx.beginPath();
-  ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-  ctx.fillStyle = "green";
-  ctx.fill();
-  ctx.closePath();
-
-  canvas.addEventListener("click", click, false);
-
+  console.log("Loading socket...");
   socket = io();
 
+  color = `rgb(${Math.floor(Math.random() * 255)},
+${Math.floor(Math.random() * 255)},
+${Math.floor(Math.random() * 255)})`;
+
+  socket.emit("new user", color);
+
+  socket.on("new user", (data) => {
+    console.log(data);
+  });
+
   socket.on("canvasUpdate", (data) => {
-    const {x, y} = data;
+    const { x, y, color } = data;
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = "green";
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
   });
-});
+}
 
 function click(event) {
   const x = event.pageX - canvas.offsetLeft;
   const y = event.pageY - canvas.offsetTop;
 
-  socket.emit("click", {x, y});
+  socket.emit("click", { x, y, color });
 
   ctx.beginPath();
   ctx.arc(x, y, 20, 0, Math.PI * 2, false);
-  ctx.fillStyle = "green";
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
--- server.js
+++ server.js
@@ -8,17 +8,24 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
   socket.on("click", (data) => {
     const { x, y } = data;
     console.log("message: " + data);
-    io.emit("canvasUpdate", data);
+    socket.broadcast.emit("canvasUpdate", data);
     console.log(`${x} ${y}`);
   });
-  socket.broadcast.emit("a new user joined!!!");
+
+  socket.on("new user", (data) => {
+    console.log("a user connected");
+    console.log(data);
+    socket.broadcast.emit("new user", data);
+  });
+
 });
 
 const PORT = 9500;
