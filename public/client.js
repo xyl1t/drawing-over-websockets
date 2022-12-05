71fdde0e62a4743e623bc3137f13904e006e4a99
Marat Isaw <marat.isaw23@gmail.com>
2022-12-06 00:21:26 +0100

    infinte scroll + cursors + preview over sockets and much more

--- public/client.js
+++ public/client.js
@@ -1,17 +1,23 @@
 let ctx;
 let socket;
 let components = [];
+let preview;
+let previews = {};
 let currentTool;
 let currentColor;
-let preview;
-
-$(() => {
-  setup();
-  loop();
-});
+let fillComponents;
+let mouseX = 0;
+let mouseY = 0;
+let scrollX = 0;
+let scrollY = 0;
+let myId;
+let myCursor = {};
+let cursors = {};
 
 class Tool {
   constructor(type) {
+    this.curId = myId;
+    this.id = genId();
     this.type = type;
   }
 
@@ -33,7 +39,7 @@ class Circle extends Tool {
     this.rx = 0;
     this.ry = 0;
     this.color = currentColor;
-    preview = this;
+    this.fill = fillComponents;
   }
   mousemove(e) {
     if (e.leftDown) {
@@ -55,11 +61,7 @@ class Circle extends Tool {
       this.cy = startY + this.ry;
     }
   }
-  mouseup(e) {
-    socket.emit("new component", this);
-    components.push(this);
-    preview = undefined;
-  }
+  mouseup(e) {}
 }
 
 class Square extends Tool {
@@ -73,7 +75,7 @@ class Square extends Tool {
     this.width = 0;
     this.height = 0;
     this.color = currentColor;
-    preview = this;
+    this.fill = fillComponents;
   }
   mousemove(e) {
     if (e.leftDown) {
@@ -81,11 +83,7 @@ class Square extends Tool {
       this.height = e.y - this.y;
     }
   }
-  mouseup(e) {
-    socket.emit("new component", this);
-    components.push(preview);
-    preview = undefined;
-  }
+  mouseup(e) {}
 }
 
 class Line extends Tool {
@@ -99,7 +97,6 @@ class Line extends Tool {
     this.ex = e.x;
     this.ey = e.y;
     this.color = currentColor;
-    preview = this;
   }
   mousemove(e) {
     if (e.leftDown) {
@@ -107,39 +104,65 @@ class Line extends Tool {
       this.ey = e.y;
     }
   }
-  mouseup(e) {
-    socket.emit("new component", this);
-    components.push(preview);
-    preview = undefined;
-  }
+  mouseup(e) {}
 }
 
 function draw(tool) {
+  if (!tool) return;
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.lineCap = "round";
+  ctx.fillStyle = tool.color;
+  ctx.strokeStyle = tool.color;
+  ctx.translate(-scrollX, -scrollY);
   switch (tool?.type) {
     case "circle":
-      ctx.beginPath();
-      // ctx.arc(tool.x, tool.y, tool.radius, 0, Math.PI * 2, false);
       ctx.ellipse(tool.cx, tool.cy, tool.rx, tool.ry, 0, 0, Math.PI * 2, false);
-      ctx.fillStyle = tool.color;
-      ctx.fill();
-      ctx.closePath();
       break;
     case "square":
-      ctx.beginPath();
       ctx.rect(tool.x, tool.y, tool.width, tool.height);
-      ctx.fillStyle = tool.color;
-      ctx.fill();
       break;
     case "line":
-      ctx.beginPath();
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = tool.color;
       ctx.moveTo(tool.sx, tool.sy);
       ctx.lineTo(tool.ex, tool.ey);
-      ctx.stroke();
       break;
   }
+
+  if (tool?.fill) ctx.fill();
+  else ctx.stroke();
+
+  // Reset current transformation matrix to the identity matrix
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
+
+function drawCursor(cursor) {
+  if (!cursor) return;
+
+  ctx.beginPath();
+  ctx.strokeStyle = cursor.color;
+  ctx.lineWidth = 2;
+  ctx.lineCap = "square";
+  // ctx.arc(cursor.x, cursor.y, 10, 0, 2 * Math.PI);
+  ctx.moveTo(cursor.x + 5 * 0 - scrollX, cursor.y + 5 * 0 - scrollY);
+  ctx.lineTo(cursor.x + 5 * 0 - scrollX, cursor.y + 5 * 3 - scrollY);
+  ctx.lineTo(cursor.x + 5 * 0.8 - scrollX, cursor.y + 5 * 2 - scrollY);
+  ctx.lineTo(cursor.x + 5 * 2 - scrollX, cursor.y + 5 * 2 - scrollY);
+  ctx.lineTo(cursor.x + 5 * 0 - scrollX, cursor.y + 5 * 0 - scrollY);
+  ctx.stroke();
+
+  ctx.fillStyle = cursor.color;
+  ctx.fillText(
+    cursor.name,
+    cursor.x - scrollX + 8,
+    cursor.y + 24 - scrollY
+  );
+}
+
+$(() => {
+  setup();
+  loop();
+});
 
 function setup() {
   // setup
@@ -150,11 +173,13 @@ function setup() {
   canvas.addEventListener("mousedown", mousedown, false);
   canvas.addEventListener("mouseup", mouseup, false);
   canvas.addEventListener("mousemove", mousemove, false);
+  canvas.addEventListener("wheel", wheel, false);
 
   ctx = canvas.getContext("2d");
 
   currentTool = new Line();
   currentColor = document.getElementById("selectedColor").value;
+  fillComponents = document.getElementById("fill").checked;
 
   $("#square").on("click", () => {
     currentTool = new Square();
@@ -171,22 +196,53 @@ function setup() {
   $("#selectedColor").on("change", () => {
     currentColor = document.getElementById("selectedColor").value;
   });
-
-  console.log("Loading socket...");
-  socket = io();
-
-  socket.emit("new user", "stub");
-
-  socket.on("new user", (data) => {
-    console.log(data);
+  $("#fill").on("change", () => {
+    fillComponents = document.getElementById("fill").checked;
+  });
+  $("#origin").on("click", () => {
+    scrollX = 0;
+    scrollY = 0;
+    $("#coordinates").text(`${scrollX}, ${scrollY}`);
+  });
+  $("#name").on("change", () => {
+    myCursor.name = document.getElementById("name").value;
+    socket.emit("cursorUpdate", myCursor);
   });
 
-  socket.on("update canvas", (data) => {
-    components = data;
+  console.log("Loading socket...");
+  socket = io({
+    auth: {
+      token: "actualUser",
+    },
+  });
+
+  socket.on("updateCanvas", (comps, prevs) => {
+    components = comps;
+    previews = prevs;
+  });
+
+  socket.on("updateCursors", (curs) => {
+    cursors = curs;
+  });
+
+  socket.on("yourCursor", (cur) => {
+    myId = cur.id;
+    myCursor = cur;
+    currentTool.curId = myId;
+
+    // set stroke/fill color to color of cursor
+    document.getElementById("selectedColor").value = rgbStringToHex(cur.color);
+    currentColor = cur.color;
   });
 }
 
 function loop() {
+  const input = document.getElementById("name").value;
+  if (input != myCursor.name) {
+    myCursor.name = input;
+    socket.emit("cursorUpdate", myCursor);
+  }
+
   ctx.fillStyle = "#eee";
   ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
@@ -196,29 +252,109 @@ function loop() {
 
   draw(preview);
 
+  for (const id in previews) {
+    if (previews[id].curId != myId) {
+      draw(previews[id]);
+    }
+  }
+
+  for (const id in cursors) {
+    if (id != myId) drawCursor(cursors[id]);
+    // drawCursor(cursors[id]);
+  }
+
   window.requestAnimationFrame(loop);
 }
 
 function mousedown(e) {
-  e.x = e.pageX - canvas.offsetLeft;
-  e.y = e.pageY - canvas.offsetTop;
-  currentTool.mousedown(e);
+  let customEvent = { ...e };
+  customEvent.x = e.pageX - canvas.offsetLeft + scrollX;
+  customEvent.y = e.pageY - canvas.offsetTop + scrollY;
+  currentTool.mousedown(customEvent);
+  preview = currentTool;
+  socket.emit("pushPreview", preview);
 }
+
 function mouseup(e) {
-  e.x = e.pageX - canvas.offsetLeft;
-  e.y = e.pageY - canvas.offsetTop;
-  currentTool.mouseup(e);
+  let customEvent = { ...e };
+  customEvent.x = e.pageX - canvas.offsetLeft + scrollX;
+  customEvent.y = e.pageY - canvas.offsetTop + scrollY;
+  currentTool.mouseup(customEvent);
+  socket.emit("popPreview", preview);
+  socket.emit("addComponent", preview);
+  components.push(preview);
+  preview = undefined;
 }
+
 function mousemove(e) {
-  e.x = e.pageX - canvas.offsetLeft;
-  e.y = e.pageY - canvas.offsetTop;
-  e.leftDown = e.buttons == 1;
-  e.rightDown = e.buttons == 2;
-  currentTool.mousemove(e);
+  mouseX = e.pageX - canvas.offsetLeft;
+  mouseY = e.pageY - canvas.offsetTop;
+  $("#coordinates").text(`${scrollX + mouseX}, ${scrollY + mouseY}`);
+
+  if (myId) {
+    myCursor.x = mouseX + scrollX;
+    myCursor.y = mouseY + scrollY;
+    socket.emit("cursorUpdate", myCursor);
+  }
+
+  let customEvent = { ...e };
+  customEvent.x = mouseX + scrollX;
+  customEvent.y = mouseY + scrollY;
+  customEvent.leftDown = e.buttons == 1;
+  customEvent.rightDown = e.buttons == 2;
+  currentTool.mousemove(customEvent);
+
+  if (customEvent.leftDown) {
+    socket.emit("popPreview", preview);
+    socket.emit("pushPreview", preview);
+  }
+}
+
+function wheel(e) {
+  e.preventDefault();
+
+  const clamp = (num) => Math.round(Math.max(-1, Math.min(1, num)));
+
+  let deltaX = clamp(e.deltaX);
+  let deltaY = clamp(e.deltaY);
+
+  if (e.shiftKey) {
+    deltaY = 0;
+    deltaX = clamp(e.deltaY || e.deltaX);
+  }
+
+  scrollX += deltaX * 50;
+  scrollY += deltaY * 50;
+
+  if (myId) {
+    myCursor.x = mouseX + scrollX;
+    myCursor.y = mouseY + scrollY;
+    socket.emit("cursorUpdate", myCursor);
+  }
+
+  $("#coordinates").text(`${scrollX + mouseX}, ${scrollY + mouseY}`);
 }
 
 function getRandomColor() {
   return `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
     Math.random() * 255
   )},${Math.floor(Math.random() * 255)})`;
+}
+
+function rgbStringToHex(rgb) {
+  let a = rgb.split("(")[1].split(")")[0];
+  a = a.split(",");
+  var b = a.map(function (x) {
+    //For each array element
+    x = parseInt(x).toString(16); //Convert to a base16 string
+    return x.length == 1 ? "0" + x : x; //Add zero if we get only one character
+  });
+  b = "#" + b.join("");
+  return b;
+}
+
+function genId() {
+  return Math.floor((1 + Math.random()) * 0x100000000)
+    .toString(16)
+    .substring(1);
 }
--- server.js
+++ server.js
@@ -8,37 +8,83 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 const components = [];
+const previews = {};
+const cursors = {};
+const sockets = {};
 
 io.on("connection", (socket) => {
+  const token = socket.handshake.auth.token;
+  if (token != "actualUser") socket.disconnect();
+
+  console.log("a new cursor connected");
+  const newCursor = {
+    id: socket.id,
+    name: "",
+    color: getRandomColor(),
+    x: 0,
+    y: 0,
+  };
+  cursors[newCursor.id] = newCursor;
+  console.log(cursors);
+  socket.emit("yourCursor", newCursor);
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("cursor disconnected");
+    delete cursors[socket.id];
+    console.log(cursors);
+    // cursors.splice(
+    //   cursors.findIndex((c) => c.socketId == c.socketId),
+    //   1
+    // );
+    // io.emit("sendCursorsInfo", cursors);
   });
 
-  socket.on("new user", (data) => {
-    console.log("a new user connected");
-    console.log(data);
-    socket.broadcast.emit("new user", data);
-  });
+  // socket.on("update cursor position", (cursor) => {
+  //   cursors[cursors.findIndex(c => c.id == cursor.id)] = cursor;
+  //   socket.broadcast.emit("sendCursorsInfo", cursors);
+  // })
 
-  socket.on("new component", (data) => {
+  socket.on("addComponent", (data) => {
     const { x, y, color } = data;
     components.push(data);
-    // if (components.length > 10) components.shift();
-    console.log('new component:', data);
+    // console.log("added component", data);
   });
-  socket.on("test", (data) => {
-    console.log(data)
+
+  socket.on("pushPreview", (p) => {
+    previews[p.id] = p;
+    console.log(previews)
   })
+  socket.on("popPreview", (p) => {
+    delete previews[p.id];
+    console.log(previews)
+  })
+
+  // socket.on("removeComponent", (data) => {
+  // });
+
+  socket.on("cursorUpdate", (cursor) => {
+    if (!cursors[cursor.id]) return;
+    cursors[cursor.id] = cursor;
+  });
 });
 
-
-function sendUpdate() {
-  io.emit("update canvas", components);
+function updateCanvas() {
+  io.emit("updateCanvas", components, previews);
+}
+function updateCursors() {
+  io.emit("updateCursors", cursors);
 }
 
-setInterval(sendUpdate, 50);
+setInterval(updateCanvas, 1000 / 20);
+setInterval(updateCursors, 1000 / 60);
 
 const PORT = 9500;
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
 });
+
+function getRandomColor() {
+  return `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
+    Math.random() * 255
+  )},${Math.floor(Math.random() * 255)})`;
+}
